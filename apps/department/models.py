22a609848197eaf46978ad0ba49e62b352599e4e
Carlos Eduardo <carloseduardo.rocha.f@gmail.com>
2021-08-05 19:31:14 -0300

    add app department

---
+++ apps/department/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Department(models.Model):
+    class Meta:
+        verbose_name = "Departamento"
+        verbose_name_plural = "Departamentos"
+
+    name = models.CharField(max_length=70)
+
+    def __str__(self) -> str:
+        return self.name
