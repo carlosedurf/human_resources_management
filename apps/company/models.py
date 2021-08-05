f11f7b94833d5fb885b15637a412f13b950be2f6
Carlos Eduardo <carloseduardo.rocha.f@gmail.com>
2021-08-05 08:19:50 -0300

    update gitignore

--- apps/company/models.py
+++ apps/company/models.py
@@ -2,4 +2,7 @@ from django.db import models
 
 
 class Company(models.Model):
+    class Meta:
+        verbose_name = "Empresa"
+        verbose_name_plural = "Empresas"
     name = models.CharField(max_length=100, help_text="Nome da Empresa")
